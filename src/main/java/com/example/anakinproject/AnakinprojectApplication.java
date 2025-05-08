4d1fa99deb88d883d98fe1c0a74bf8f826f478c9
Anakin29 <asdrubal.a.b.l@gmail.com>
2025-05-08 15:12:39 +0200

    Add files via upload

---
+++ src/main/java/com/example/anakinproject/AnakinprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.anakinproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnakinprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnakinprojectApplication.class, args);
+	}
+
+}
